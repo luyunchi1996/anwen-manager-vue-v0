c34804d6d40e167e46b3c93db89c11b6e59095f4
chi_lu <18013753653@163.com>
2019-07-22 22:10:55 +0800

    #

---
+++ src/store/modules/cart.js
@@ -0,0 +1,29 @@
+
+// initial state
+// shape: [{ id, quantity }]
+const state = {
+    items: [],
+    checkoutStatus: null
+}
+
+// getters
+const getters = {
+}
+
+// actions
+const actions = {
+
+}
+
+// mutations
+const mutations = {
+
+}
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations
+}
